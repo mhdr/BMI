d7b9816fe3feb9cc0121e1922e70ca938a592dc7
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2017-04-21 22:13:43 +0430

    application icon is set bottom navigation is working now navigation drawer can be opened from right side

---
+++ BMI/app/src/main/java/ir/mhdr/bmi/MainActivity.java
@@ -0,0 +1,73 @@
+package ir.mhdr.bmi;
+
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ViewCompat.setLayoutDirection(toolbar, ViewCompat.LAYOUT_DIRECTION_RTL);
+        getSupportActionBar().setTitle(R.string.app_name_fa);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // it will not work for right to left navigation drawer
+/*        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }*/
+
+
+        // so we have to open and close the navigation drawer ourselves
+        if(item.getItemId() == android.R.id.home) {
+            if(drawerLayout.isDrawerOpen(Gravity.END)) {
+                drawerLayout.closeDrawer(Gravity.END);
+            }
+            else {
+                drawerLayout.openDrawer(Gravity.END);
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
