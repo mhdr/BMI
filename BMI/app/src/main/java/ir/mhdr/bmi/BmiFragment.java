f57c4c15ce08d7a974567ff9b5bc38fbd3b28dff
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2017-05-12 21:04:38 +0430

    nothing special

--- BMI/app/src/main/java/ir/mhdr/bmi/BmiFragment.java
+++ BMI/app/src/main/java/ir/mhdr/bmi/BmiFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
