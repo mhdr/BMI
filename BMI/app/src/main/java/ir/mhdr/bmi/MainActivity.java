cc25b91897a558b7dd6ba497d9822a6b41c1765a
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2017-05-28 22:48:11 +0430

    working on spinner of profile

--- BMI/app/src/main/java/ir/mhdr/bmi/MainActivity.java
+++ BMI/app/src/main/java/ir/mhdr/bmi/MainActivity.java
@@ -3,22 +3,33 @@ package ir.mhdr.bmi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ScrollingTabContainerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ir.mhdr.bmi.bl.UserBL;
 import ir.mhdr.bmi.lib.CustomViewPager;
 import ir.mhdr.bmi.lib.MainViewPagerAdapter;
+import ir.mhdr.bmi.model.User;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,7 +39,12 @@ public class MainActivity extends AppCompatActivity {
     ActionBarDrawerToggle actionBarDrawerToggle;
     BottomNavigationView bottomNavigationView;
     CustomViewPager viewPagerMain;
+    NavigationView navigationView;
     boolean firstRun = false;
+    Spinner spinnerProfile;
+    ArrayAdapter<String> spinnerAdapter;
+
+    List<User> profileList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish(); // to close current activity
         }
+
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        ViewCompat.setLayoutDirection(navigationView, ViewCompat.LAYOUT_DIRECTION_RTL);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -74,7 +93,78 @@ public class MainActivity extends AppCompatActivity {
         // bind bottomNavigationView and viewPager
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationView_OnNavigationItemSelectedListener);
         viewPagerMain.addOnPageChangeListener(viewPagerMain_OnPageChangeListener);
+
+        navigationView.setNavigationItemSelectedListener(navigationView_OnNavigationItemSelectedListener);
+
+        spinnerProfile = (Spinner) navigationView.getMenu().findItem(R.id.itemMenuSelectProfile).getActionView();
+        generateSpinnerProfile();
+        spinnerProfile.setOnItemSelectedListener(spinnerProfile_OnItemSelectedListener);
     }
+
+    AdapterView.OnItemSelectedListener spinnerProfile_OnItemSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+
+    private void generateSpinnerProfile() {
+        UserBL userBL = new UserBL(MainActivity.this);
+        profileList = userBL.getUsers();
+        List<String> profileStrList = new ArrayList<>();
+
+        String[] profiles;
+
+        for (User u : profileList) {
+            profileStrList.add(u.getName());
+        }
+
+        profiles = profileStrList.toArray(new String[0]);
+        spinnerAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item,
+                profiles);
+
+        spinnerProfile.setAdapter(spinnerAdapter);
+    }
+
+    NavigationView.OnNavigationItemSelectedListener navigationView_OnNavigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            // uncheck other menus and sub menus
+            int menuSize = navigationView.getMenu().size();
+
+            for (int i = 0; i < menuSize; i++) {
+                MenuItem menuItem = navigationView.getMenu().getItem(i);
+
+                if (menuItem.hasSubMenu()) {
+                    int submenuSize = menuItem.getSubMenu().size();
+
+                    for (int j = 0; j < submenuSize; j++) {
+                        MenuItem subItem = menuItem.getSubMenu().getItem(j);
+                        subItem.setChecked(false);
+                    }
+                } else {
+                    menuItem.setChecked(false);
+                }
+            }
+
+            item.setChecked(true);
+
+            if (item.getItemId() == R.id.itemMenuExit) {
+
+                finish();
+                System.exit(0);
+                return true;
+            }
+
+            return false;
+        }
+    };
 
     BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationView_OnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
