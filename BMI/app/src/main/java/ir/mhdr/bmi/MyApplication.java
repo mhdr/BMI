9950c170fc8b45007ac9ed8e6c3dcb5d6f10d6d0
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2017-06-16 17:41:12 +0430

    close issue #24 : if language of device is rtl all views changes it direction

--- BMI/app/src/main/java/ir/mhdr/bmi/MyApplication.java
+++ BMI/app/src/main/java/ir/mhdr/bmi/MyApplication.java
@@ -1,10 +1,15 @@
 package ir.mhdr.bmi;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.multidex.MultiDexApplication;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 import net.time4j.android.ApplicationStarter;
+
+import java.util.Locale;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
@@ -22,6 +27,21 @@ public class MyApplication extends MultiDexApplication {
 
         ApplicationStarter.initialize(this, true); // time4j
         JodaTimeAndroid.init(this); // joda
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        Resources res = base.getResources();
+
+        Locale locale = new Locale("en");
+        Locale.setDefault(locale);
+
+        Configuration config = new Configuration();
+        config.locale = locale;
+
+        res.updateConfiguration(config, res.getDisplayMetrics());
+
+        super.attachBaseContext(base);
     }
 }
 
